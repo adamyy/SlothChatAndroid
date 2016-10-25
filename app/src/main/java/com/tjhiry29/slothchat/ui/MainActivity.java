faa02e2d5d704a02b78e25d74534fcb2e2af886a
yifan <y454yang@gmail.com>
2016-10-24 21:16:15 -0400

    initial commit

---
+++ app/src/main/java/com/tjhiry29/slothchat/ui/MainActivity.java
@@ -0,0 +1,25 @@
+package com.tjhiry29.slothchat.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tjhiry29.slothchat.BuildConfig;
+import com.tjhiry29.slothchat.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String SERVER_URL = BuildConfig.SERVER_URL;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+    }
+
+
+}
